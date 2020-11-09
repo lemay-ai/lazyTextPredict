a741644821983d9a594526b67fb68b91c13908b8
Ned Booker <63317915+epb378@users.noreply.github.com>
2020-11-08 19:33:13 -0500

    Add setup requirements to setup.py
    added transformers and nlp as required packages in the install file - may need to add specific version numbers down the line

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lemay-ai/lazyTextPredict",
     packages=setuptools.find_packages(),
+    install_requires =[
+    "transformers",
+    "nlp"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
