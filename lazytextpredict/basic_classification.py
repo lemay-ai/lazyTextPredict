cadd502d3f00a709c5cb937d3a3b6988a151a23d
Ned Booker <63317915+epb378@users.noreply.github.com>
2020-12-07 09:48:05 -0500

    Add user data
    User can submit custom data for binary classification. Need to work on multilabel classifications - may need to restructure a little bit

--- lazytextpredict/basic_classification.py
+++ lazytextpredict/basic_classification.py
@@ -13,32 +13,61 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from random import sample, choices
 from joblib import dump, load
+def string_labels_to_int(Y): 
+  keys={}
+  new_Y=[]
+  for item in Y:
+    if item in keys:
+      new_Y.append(keys[item])
+    else:
+      keys.update({item:len(keys)+1})
+      new_Y.append(keys[item])
+  return new_Y, keys
+
+def int_labels_to_list(Y,keys):
+  new_Y=[]
+  for item in Y:
+    sublist=[0] * len(keys)
+    sublist[item-1]=1
+    sublist=torch.tensor(sublist)
+    new_Y.append(sublist)
+  return new_Y
+
+
 
 class LTP:
 	def __init__ (self, Xdata=None,Ydata=None):
 		self.model_list = [
-			'linear_SVM',
-			'multinomial_naive_bayesian',
 			'bert-base-uncased',
 			'albert-base-v2',
-			'roberta-base'
+			'roberta-base',
+			'linear_SVM',
+			'multinomial_naive_bayesian',
 
 			]
 		if Xdata==Ydata==None or (Xdata==None and Ydata!=None) or (Xdata!=None and Ydata==None):
-      print('Either you have not put in your own data, or you have only put in X or Y data, loading default dataset...')
-      self.train_dataset_raw, self.test_dataset_raw = load_dataset('imdb', split=['train', 'test'])
-      X=self.train_dataset_raw['text']+self.test_dataset_raw['text']
-      Y=self.train_dataset_raw['label']+self.test_dataset_raw['label']
-    else:
-      X=Xdata
-      Y=Ydata
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
+			print('Either you have not put in your own data, or you have only put in X or Y data, loading default dataset...')
+			self.train_dataset_raw, self.test_dataset_raw = load_dataset('imdb', split=['train', 'test'])
+			X=self.train_dataset_raw['text']+self.test_dataset_raw['text']
+			Y=self.train_dataset_raw['label']+self.test_dataset_raw['label']
+		else:
+			X=Xdata
+			Y=Ydata
+			if all(isinstance(n, int) for n in Y):
+				pass
+			else:
+				Y,keys=string_labels_to_int(Y)
+				
+		X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
                                                         stratify=Y, 
-                                                        test_size=0.005,
-                                                        train_size=0.005)
-    self.train_dataset_raw = Dataset.from_pandas(pd.DataFrame({'text':X_train, 'label': Y_train}))
-    self.test_dataset_raw = Dataset.from_pandas(pd.DataFrame({'text':X_test, 'label': Y_test}))
-    self.all_metrics = {}
+                                                        test_size=0.05,
+                                                        train_size=0.05)
+		#self.num_labels=len(keys)
+		#self.train_dataset_raw_CNN = TensorDataset(X_train, int_labels_to_list(Y_train,keys))
+		#self.test_dataset_raw_CNN = TensorDataset(X_test, int_labels_to_list(Y_test,keys))
+		self.train_dataset_raw = Dataset.from_pandas(pd.DataFrame({'text':X_train, 'label': Y_train}))
+		self.test_dataset_raw = Dataset.from_pandas(pd.DataFrame({'text':X_test, 'label': Y_test}))	
+		self.all_metrics = {}
 
 
 
@@ -81,12 +110,12 @@ class LTP:
 
 			training_args = TrainingArguments(
 			output_dir='./results/'+model_name,
-			num_train_epochs=1,
+			num_train_epochs=10,
 			per_device_train_batch_size=16,
 			per_device_eval_batch_size=64,
 			warmup_steps=500,
 			weight_decay=0.01,
-			evaluate_during_training=True,
+			#evaluate_during_training=True,
 			logging_dir='./logs/'+model_name,
 			)
 
@@ -94,14 +123,14 @@ class LTP:
 			tokenizer = None
 
 			if model_name == "bert-base-uncased":
-				model = BertForSequenceClassification.from_pretrained(model_name)
+				model = BertForSequenceClassification.from_pretrained(model_name, num_labels=2)#self.num_labels)
 				tokenizer = BertTokenizerFast.from_pretrained(model_name)
 			elif model_name == "albert-base-v2":
 				tokenizer = transformers.AlbertTokenizer.from_pretrained('albert-base-v2')
-				model = transformers.AlbertForSequenceClassification.from_pretrained('albert-base-v2', return_dict=True)
+				model = transformers.AlbertForSequenceClassification.from_pretrained('albert-base-v2', return_dict=True, num_labels=2)#self.num_labels)
 			elif model_name == "roberta-base":
 				tokenizer = transformers.RobertaTokenizer.from_pretrained('roberta-base')
-				model = transformers.RobertaForSequenceClassification.from_pretrained('roberta-base', return_dict=True)
+				model = transformers.RobertaForSequenceClassification.from_pretrained('roberta-base', return_dict=True, num_labels=2)#self.num_labels)
 			elif model_name == "linear_SVM":
 				tokenizer = None
 				model = 'linear_SVM'
@@ -131,10 +160,11 @@ class LTP:
 			def tokenize(batch):
 				return tokenizer(batch['text'], padding=True, truncation=True)
 
-
+			def 
 
 
 			if tokenizer is not None:
+
 				train_dataset = self.train_dataset_raw.map(tokenize, batched=True, batch_size=len(self.train_dataset_raw))
 				test_dataset = self.test_dataset_raw.map(tokenize, batched=True, batch_size=len(self.train_dataset_raw))
 				train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
@@ -158,7 +188,7 @@ class LTP:
 				pipeline.fit(train_dataset['text'], train_dataset['label'])
 				
 				prediction=pipeline.predict(test_dataset['text'])
-				precision, recall, f1, _ = precision_recall_fscore_support(test_dataset['label'], prediction, average='binary')
+				precision, recall, f1, _ = precision_recall_fscore_support(test_dataset['label'], prediction, average=None)
 				full_report=classification_report(test_dataset['label'], prediction)
 				acc = accuracy_score(test_dataset['label'], prediction)
 				loss=hamming_loss(test_dataset['label'], prediction)
@@ -198,6 +228,16 @@ class LTP:
 			torch.cuda.empty_cache()
 	def predict(self,text):
 		for model_name in self.model_list:
+			if model_name == "linear_SVM" or model_name == "multinomial_naive_bayesian":
+				clf = load('/content/'+model_name+'_model.joblib')
+				y=clf.predict([text])
+				print(y)
+			else:
+				if model_name == "bert-base-uncased":
+					model = BertForSequenceClassification.from_pretrained('/content/bert-base-uncased_model')
+					#tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased_model')
+…				except:
+					print('I have not got this to work yet, it was probably a good review')elf.model_list:
 			if model_name == "linear_SVM" or model_name == "multinomial_naive_bayesian":
 				clf = load('/content/'+model_name+'_model.joblib')
 				y=clf.predict([text])
