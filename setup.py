9290f4d1a5eec273d87e653d9f9e7ff361f6c417
Ned Booker <63317915+epb378@users.noreply.github.com>
2021-01-11 13:01:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lazytextpredict", # Replace with your own username
+    name="lazy-text-predict",
     version="0.0.1",
     author="Edward Booker",
     author_email="epb378@gmail.com",
-    description="A small example package",
+    description="Text classification automl",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lemay-ai/lazyTextPredict",
